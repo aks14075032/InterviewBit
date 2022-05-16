714df3cd94a643002e0db2e4103d4f167d3a437b
apli.ai <akumbarwar18@gmail.com>
2022-05-16 14:42:06 +0530

    Can Sum Tabular

---
+++ Dynamic Programming/canSumTabular.py
@@ -0,0 +1,16 @@
+def canSum(targetSum, Numbers):
+    table = [False for i in range(targetSum+2)]
+    table[0]=True
+
+    for i in range(0,targetSum+2):
+        if table[i] == True:
+            for num in Numbers:
+                if i + num <= targetSum+1:
+                    table[i+num] = True
+
+    return table[targetSum]
+
+print(canSum(10,[1,2,3,5,6,7]))
+print(canSum(7,[2,4])) 
+print(canSum(7,[2,3]))
+print(canSum(300,[7,14]))
